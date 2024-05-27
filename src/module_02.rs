ba470863b052e8099a4e0f6515b8fe7995d83910
Md. Abdullah Al Mahmud <shaphil.mahmud@gmail.com>
2024-05-28 02:41:10 +0600

    Add Module 02 - Discovering Rust, Lesson 04 - Functions

--- src/module_02.rs
+++ src/module_02.rs
@@ -1,9 +1,12 @@
 pub mod discovering_rust {
+    fn add(a: i32, b: i32) -> i32 {
+        a + b
+    }
+
     pub fn variables_and_mutability() {
         let a = 38;
         let b = 4;
-        let mut sum = a;
-        sum += b;
+        let sum = add(a, b);
 
         println!("{} + {} = {}", a, b, sum);
     }
