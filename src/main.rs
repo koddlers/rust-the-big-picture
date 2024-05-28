6d26480113af46f65505b525df6c0e69a4f19bd6
Md. Abdullah Al Mahmud <shaphil.mahmud@gmail.com>
2024-05-28 19:10:45 +0600

    Add Module 03 - Functional Flavored OOP, Lesson 02 - Closures

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
 #![allow(unused)]
 
 mod module_02;
+mod module_03;
 
 use module_02::discovering_rust;
+use module_03::functional_flavored_oop;
 
 fn main() {
+    // Module 02 - Discovering Rust
     // discovering_rust::variables_and_mutability();
     // discovering_rust::arrays_ranges_and_looping();
     // discovering_rust::array_slices_and_zero_cost_abstractions();
-    discovering_rust::vectors();
+    // discovering_rust::vectors();
+
+    // Module 03 - Functional Flavored Object-Oriented Programming
+    // functional_flavored_oop::closures();
+    functional_flavored_oop::capturing_closures();
 }
---
+++ src/module_03.rs
@@ -0,0 +1,42 @@
+pub mod functional_flavored_oop {
+    pub fn closures() {
+        let add = |a, b| a + b;
+
+        let sum_range = |from, to| {
+            let mut sum = 0;
+
+            for i in from..to {
+                // this closure captures the `add` variable from the
+                // enclosing scope, which is another closure
+                sum = add(sum, i);
+            }
+
+            // return value of this closure
+            sum
+        };
+
+        println!("sum = {}", sum_range(3, 10));
+    }
+
+    pub fn capturing_closures() {
+        let add = |a, b| a + b;
+
+        // tuple destructuring
+        let (from, to) = (3, 10);
+        let sum_range = || {
+            let mut sum = 0;
+
+            // the variables `from` and `to` are also captured from the enclosing scope
+            for i in from..to {
+                // this closure captures the `add` variable from the
+                // enclosing scope, which is another closure
+                sum = add(sum, i);
+            }
+
+            // return value of this closure
+            sum
+        };
+
+        println!("sum = {}", sum_range());
+    }
+}
